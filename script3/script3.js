83e3de94103eb90df59b4707793e0e26134e1d66
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2020-10-22 11:11:34 -0400

    Create a form and populate a table

---
+++ script3/script3.js
@@ -0,0 +1,80 @@
+// class MyArray {
+  //     content = ["Mikhael", "Angélica"];
+  
+  //     find(callback) {
+    //         for (let i = 0; i < this.content.length; i++) {
+      //             const element = this.content[i];
+      
+      //             const isTrue = callback(element);
+      
+      //             if (isTrue)
+      //                 return element;
+      //             else
+//                 return undefined;
+//         }
+//     }
+// }
+
+const people = [];
+
+const saveButton = document.getElementById("save-button");
+saveButton.onclick = onSaveButton;
+
+function onSaveButton() {
+    const name = document.getElementById("name").value;
+    const surnames = document.getElementById("surnames").value;
+    const birthdate = document.getElementById("birthdate").value;
+
+    function findIfPersonExists(person) {
+        const nameExists = person.name === name;
+        const surnamesExists = person.surnames === surnames;
+
+        const personExists = nameExists && surnamesExists;
+
+        return personExists;
+    }
+
+    const personExists = people.find(findIfPersonExists);
+
+    if (personExists)
+        return;
+
+    const newPerson = {
+        name,
+        surnames,
+        birthdate
+    };
+
+    // Storing the data.
+    people.push(newPerson);
+
+    // Updating the table (UI).
+    updateTable(newPerson);
+}
+
+function updateTable(newPerson) {
+    // Table.
+    const tableElement = document.querySelector("table");
+
+    // Table body.
+    const tbodyElement = tableElement.tBodies[0];
+
+    // Make a row.
+    const rowElement = document.createElement("tr");
+
+    // Make the columns.
+    const nameColumn = document.createElement("td");
+    nameColumn.textContent = newPerson.name;
+
+    const surnamesColumn = document.createElement("td");
+    surnamesColumn.textContent = newPerson.surnames;
+
+    const birthdateColumn = document.createElement("td");
+    birthdateColumn.textContent = newPerson.birthdate;
+
+    rowElement.appendChild(nameColumn);
+    rowElement.appendChild(surnamesColumn);
+    rowElement.appendChild(birthdateColumn);
+
+    tbodyElement.appendChild(rowElement);
+}
